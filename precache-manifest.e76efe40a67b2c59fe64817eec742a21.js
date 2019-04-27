cee93b159e18196b49d9a15e69d6cf4bde394afc
UCLAShawn3G <uclashawn1@gmail.com>
2019-04-27 13:43:34 -0700

    Updates

---
+++ precache-manifest.e76efe40a67b2c59fe64817eec742a21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c21056af16f403205e2b6ed5ec4acc",
+    "url": "/clickGame/index.html"
+  },
+  {
+    "revision": "8d58cf31324bfb87a835",
+    "url": "/clickGame/static/css/main.f18132f6.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "8d58cf31324bfb87a835",
+    "url": "/clickGame/static/js/main.634ce868.chunk.js"
+  },
+  {
+    "revision": "2cccf4db22d9fda060a7",
+    "url": "/clickGame/static/js/runtime~main.7080151e.js"
+  }
+]);
